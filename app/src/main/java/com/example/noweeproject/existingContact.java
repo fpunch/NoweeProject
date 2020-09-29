7a75f21cbaee8f6a53c20d288a7b3e6d14aae0e0
Jesse <19328950@students.latrobe.edu.au>
2020-09-29 15:41:39 +1000

    New changes: Added existingContact as well as newContact and the XML's for each as well. There are also some additions to string.xml for these activities.
    29/09/2020

---
+++ app/src/main/java/com/example/noweeproject/existingContact.java
@@ -0,0 +1,41 @@
+package com.example.noweeproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class existingContact extends AppCompatActivity {
+
+    Button AddButton = null;
+    TextView UsernameField = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_existing_contact);
+
+        //Initialise UI Objects
+        AddButton = (Button) findViewById(R.id.AddButton);
+        UsernameField = (TextView) findViewById(R.id.Username);
+
+
+        //Listener for Add Button -> send activity
+        AddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent myIntent = new Intent(existingContact.this, SendActivity.class);
+                myIntent.putExtra();
+                existingContact.this.startActivity(myIntent);
+            }
+        });
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/noweeproject/newContact.java
@@ -0,0 +1,40 @@
+package com.example.noweeproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.EditText;
+
+public class newContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_contact);
+
+    }
+
+    //Add Contact Method
+    protected void btnAdd_Contact_OnClick(View view) {
+        Intent intent = new Intent (ContactsContract.Intents.Insert.ACTION);
+        intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+
+        // Gather Details of Contact
+        EditText mFirstName = (EditText) findViewById(R.id.editFirstName);
+        EditText mLastName = (EditText) findViewById(R.id.editSurname);
+        EditText mEmail = (EditText) findViewById(R.id.editEmail);
+        EditText mPhoneNumber = (EditText) findViewById(R.id.editPhone);
+
+        //Insert an email address
+        intent.putExtra(ContactsContract.Intents.Insert.EMAIL, mEmail.getText())
+                .putExtra(ContactsContract.Intents.Insert.EMAIL_TYPE,ContactsContract.CommonDataKinds.Email.TYPE_WORK)
+                .putExtra(ContactsContract.Intents.Insert.PHONE, mPhoneNumber.getText())
+                .putExtra(ContactsContract.Intents.Insert.PHONE_TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_WORK)
+                .putExtra(ContactsContract.Intents.Insert.NAME,"Test");
+
+        startActivity(intent);
+    }
+}
