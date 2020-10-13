51549ccc86316e9a59ae13e49a6b18e76238dd27
ailop7 <71115224+ailop7@users.noreply.github.com>
2020-10-13 18:55:04 +1100

    6:54 13/10

--- app/src/main/java/com/example/noweeproject/ChatActivity.java
+++ app/src/main/java/com/example/noweeproject/ChatActivity.java
@@ -8,24 +8,57 @@ import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ChatActivity extends AppCompatActivity {
+    static final int PICK_CONTACT = 1;
     private static final String TAG = ChatActivity.class.getSimpleName();
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
-    @Override
+    private String contact_number = null;
+    TextView numView = null;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         checkForSmsPermission();
-    }
+        Intent intent = getIntent();
+        contact_number = intent.getStringExtra("contact_num");
+        numView= findViewById(R.id.editText_main);
+        Button selectButton =  findViewById(R.id.selectContactBtn);
 
+        selectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK
+                        , ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(intent, PICK_CONTACT);
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_CONTACT && resultCode == RESULT_OK) {
+
+            Uri contactUri = data.getData();
+            Cursor cursor = getContentResolver().query(contactUri, null, null, null, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int numberIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                contact_number = cursor.getString(numberIndex);
+                Log.d("TEST", contact_number);
+                numView.setText(contact_number);
+                cursor.close();
+            }
+        }
+    }
 
     private void checkForSmsPermission() {
         if (ActivityCompat.checkSelfPermission(this,
@@ -63,8 +96,7 @@ public class ChatActivity extends AppCompatActivity {
 
 
     public void smsSendMessage(View view) {
-        EditText editText = (EditText) findViewById(R.id.editText_main);
-        String destinationAddress = editText.getText().toString();
+
 
         EditText smsEditText = (EditText) findViewById(R.id.sms_message);
 
@@ -76,8 +108,9 @@ public class ChatActivity extends AppCompatActivity {
 
         checkForSmsPermission();
         SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(destinationAddress, scAddress, smsMessage,
+        smsManager.sendTextMessage(contact_number, scAddress, smsMessage,
                 sentIntent, deliveryIntent);
+        Toast.makeText(this, "message sent!", Toast.LENGTH_SHORT).show();
     }
 
 
