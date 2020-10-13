a5c388bc80f35d2f6fa420a33b7e5f1dd8ff7a67
ailop7 <71115224+ailop7@users.noreply.github.com>
2020-10-13 15:49:43 +1100

    3:46 13/10

--- app/src/main/java/com/example/noweeproject/ChatActivity.java
+++ app/src/main/java/com/example/noweeproject/ChatActivity.java
@@ -1,16 +1,112 @@
 package com.example.noweeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import com.example.noweeproject.R;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ChatActivity extends AppCompatActivity {
-
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        setContentView(R.layout.activity_main);
+        checkForSmsPermission();
+    }
+
+
+    private void checkForSmsPermission() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, getString(R.string.permission_not_granted));
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_REQUEST_SEND_SMS);
+        } else {
+
+            enableSmsButton();
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        // For the requestCode, check if permission was granted or not.
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (permissions[0].equalsIgnoreCase(Manifest.permission.SEND_SMS)
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission was granted. Enable sms button.
+                    enableSmsButton();
+                } else {
+                    // Permission denied.
+                    Log.d(TAG, getString(R.string.failure_permission));
+                    Toast.makeText(this, getString(R.string.failure_permission),
+                            Toast.LENGTH_LONG).show();
+                    // Disable the sms button.
+                    disableSmsButton();
+                }
+            }
+        }
+    }
+
+
+    public void smsSendMessage(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText_main);
+        String destinationAddress = editText.getText().toString();
+
+        EditText smsEditText = (EditText) findViewById(R.id.sms_message);
+
+        String smsMessage = smsEditText.getText().toString();
+
+        String scAddress = null;
+
+        PendingIntent sentIntent = null, deliveryIntent = null;
+
+        checkForSmsPermission();
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(destinationAddress, scAddress, smsMessage,
+                sentIntent, deliveryIntent);
+    }
+
+
+    private void disableSmsButton() {
+        Toast.makeText(this, R.string.sms_disabled, Toast.LENGTH_LONG).show();
+        @SuppressLint("WrongViewCast")
+        Button smsButton = (Button) findViewById(R.id.message_icon);
+        smsButton.setVisibility(View.INVISIBLE);
+        Button retryButton = (Button) findViewById(R.id.button_retry);
+        retryButton.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Makes the sms button (message icon) visible so that it can be used.
+     */
+    private void enableSmsButton() {
+        @SuppressLint("WrongViewCast")
+        Button smsButton = (Button) findViewById(R.id.message_icon);
+        smsButton.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Sends an intent to start the activity.
+     *
+     * @param view  View (Retry button) that was clicked.
+     */
+    public void retryApp(View view) {
+        Intent intent = getPackageManager().getLaunchIntentForPackage(getPackageName());
+        startActivity(intent);
     }
 }
