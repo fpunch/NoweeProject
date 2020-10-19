4d47e17308c6a68f99a7590f8cd35ebe540f1d90
fpunc <18904251@students.latrobe.edu.au>
2020-10-19 12:23:38 +1100

    jx final 19/10/2020 update

--- app/src/main/java/com/example/noweeproject/MainActivity.java
+++ app/src/main/java/com/example/noweeproject/MainActivity.java
@@ -1,8 +1,5 @@
 package com.example.noweeproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +8,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         statusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              // statusGo();
+              // statusGoes();
                 startActivity(new Intent(MainActivity.this, Status.class));
             }
         });
